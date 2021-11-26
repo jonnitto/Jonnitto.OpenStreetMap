69ee3c0dc73f9c8b028f3f76ba2e16547c8d6499
Jon Uhlmann <account@uhlmann.pro>
2021-11-27 00:44:12 +0100

    Update: Use new format for coordinates

--- Resources/Private/Fusion/Presentation/index.js
+++ Resources/Private/Fusion/Presentation/index.js
@@ -20,12 +20,13 @@ const icon = divIcon({
 
 const initFunction = ({ element, live }) => {
     const settings = { ...globalSettings.mapOptions, ...JSON.parse(element.dataset?.map || null) };
-    if (!settings.center) {
-        settings.center = [0, 0];
-    }
+    settings.center = settings.center ? [settings.center.lat, settings.center.lng] : [0, 0];
+
     const canvas = getMapCanvas(element);
-    const markerGroup = [];
+    const markerCollection = [];
     const addresses = getAddresses(canvas);
+    const numberOfAddresses = addresses.length;
+    const inEditMode = !live && numberOfAddresses === 1;
 
     canvas.innerHTML = "";
 
@@ -39,8 +40,8 @@ const initFunction = ({ element, live }) => {
     tileLayer(globalSettings.tiles.url, globalSettings.tiles.options).addTo(MAP);
 
     addresses.forEach((address) => {
-        const MARKER = marker(address.coordinate, { icon, draggable: !live }).addTo(MAP);
-        markerGroup.push(MARKER);
+        const MARKER = marker([address.lat, address.lng], { icon, draggable: !live }).addTo(MAP);
+        markerCollection.push(MARKER);
         if (address.html) {
             const popup = MARKER.bindPopup(address.html, {
                 maxWidth: 500,
@@ -49,24 +50,20 @@ const initFunction = ({ element, live }) => {
                 popup.openPopup();
             }
         }
+        if (inEditMode) {
+            setTimeout(() => {
+                const EDITORS = getLatLngEditors(element);
+                if (EDITORS) {
+                    MARKER.addEventListener("move", (event) => {
+                        updateLatLngEditors(EDITORS, event.latlng);
+                    });
+                }
+            }, 10);
+        }
     });
 
-    const markerLength = markerGroup.length;
-
-    if (!live && markerLength === 1) {
-        // Wait for the Neos UI
-        setTimeout(() => {
-            const EDITORS = getLatLngEditors(element);
-            if (EDITORS) {
-                markerGroup[0].addEventListener("move", (event) => {
-                    updateLatLngEditors(EDITORS, event.latlng);
-                });
-            }
-        }, 10);
-    }
-
-    if (markerLength) {
-        MAP.fitBounds(new featureGroup(markerGroup).getBounds());
+    if (numberOfAddresses) {
+        MAP.fitBounds(new featureGroup(markerCollection).getBounds());
     } else {
         console.warn("No addresses found");
         MAP.setView(settings.center, 2);
@@ -77,12 +74,14 @@ const initFunction = ({ element, live }) => {
             detail: {
                 map: MAP,
                 element,
-                markers: markerGroup,
+                markers: markerCollection,
             },
         })
     );
 
-    element.style.visibility = "visible";
+    setTimeout(() => {
+        element.style.visibility = "visible";
+    }, 50);
 };
 
 export { initFunction, initFrontend, initBackend };
